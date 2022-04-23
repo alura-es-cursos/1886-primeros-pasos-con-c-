95cf5b8a950e5710c926a92f21d91435b31fd1cd
Leonardo <ljcl79@gmail.com>
2022-04-23 12:09:48 -0300

    Aula4

---
+++ 07-TipoDatoTexto/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_TipoDatoTexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejecutando el proyecto 07-Tipo de dato texto.");
+
+            char primeraLetra = '\t';
+            char letraComoNumero = (char)65;
+
+            letraComoNumero = (char)(letraComoNumero + 1);
+
+            Console.WriteLine("Col1" + primeraLetra + "Col2" + primeraLetra + "Col3");
+            Console.WriteLine("Col1b" + primeraLetra + "Col2b" + primeraLetra + "Col3b");
+
+
+            string nombreCurso = "Estoy en el curso de:\n"+
+            "C# Primeros pasos";
+            Console.WriteLine(nombreCurso);
+
+
+            Console.ReadLine();
+        }
+    }
+}
